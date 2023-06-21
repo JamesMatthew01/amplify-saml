ea5ff8e5b2ab8d70e40ec7f2cfdb93d45af7b58e
JamesMatthew01 <james.matthew@aprllp.com>
2023-06-21 20:41:55 +0100

    Federated sign in with Azure AD via SAML

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+import React, {useEffect, useState} from "react";
+import {Amplify, Auth, Hub} from "aws-amplify";
+import {Flex, View} from '@aws-amplify/ui-react';
 
-function App({ signOut }) {
+import "./App.css";
+
+
+Amplify.configure({
+  Auth: {
+    region: "us-east-1",
+    userPoolId: "us-east-1_FHaiC3l4F",
+    userPoolWebClientId: "5uev9n9e0njp32ik84kpb364j7",
+    oauth: {
+      domain: "jcmtest.auth.us-east-1.amazoncognito.com",
+      scope: ["email", "openid"],
+      redirectSignIn: "http://localhost:3000",
+      redirectSignOut: "http://localhost:3000",
+      responseType: "code"
+    }
+  }
+});
+
+function App() {
+  const [token, setToken] = useState(null);
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    Hub.listen("auth", ({payload: {event, data}}) => {
+      switch (event) {
+        case "signIn":
+        case "cognitoHostedUI":
+          setToken("granting...");
+          getToken().then(userToken => setToken(userToken.idToken.jwtToken));
+          setUsername(data.username);
+          break;
+        case "signOut":
+          setToken(null);
+          break;
+        case "signIn_failure":
+        case "cognitoHostedUI_failure":
+          console.log("Sign in failure", data);
+          break;
+        default:
+          break;
+      }
+    });
+  }, []);
+
+  function getToken() {
+    return Auth.currentSession()
+      .then(session => session)
+      .catch(err => console.log(err));
+  }
+
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
+    <View>
+      {token ? <div level = {1}> Welcome - {username} </div> : <div> Welcome - please sign in </div>}
+      <Flex>
+        {token ?
+          <button onClick = {() => Auth.signOut()}> Sign Out </button> : 
+          <button onClick = {() => Auth.federatedSignIn()}> Sign In </button> //optionally can add the argument {provider: "azuread"} to go straight to microsoft sign in and skip cognito hosted ui
+          }
+      </Flex>
     </View>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
